a8c50d4037cbdd9da66fbee4acf0f0542f9f1700
Yuki <yuki.kutsuya@gmail.com>
2021-04-11 22:32:00 +0200

    More fixes :o

--- AnimeActors/AnimeActors.Android/MainActivity.cs
+++ AnimeActors/AnimeActors.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using FFImageLoading.Forms.Platform;
 
 namespace AnimeActors.Droid
 {
-    [Activity(Label = "AnimeActors", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Anime Actors", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
